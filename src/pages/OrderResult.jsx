b684942d21cb623f9461fbecb8879bf7148ff2fb
TEgeYuce <tegeyucesan@gmail.com>
2025-10-22 17:43:18 +0300

    "SİRAPİŞ ALINDI" Sayfası OrderResult.jsx Dosyası İçerisinde React JSX Özellikleriyle Dinamik Olarak Yapıldı

---
+++ src/pages/OrderResult.jsx
@@ -0,0 +1,56 @@
+//import React from "react";
+import { useLocation } from "react-router";
+import Footer from "../components/Footer";
+
+const OrderResult = () => {
+  const location = useLocation();
+  const { response } = location.state || {};
+
+  return (
+    <div className="siparisSonucFullscreen">
+      <div className="siparisSonucContainer">
+        <img src="\images\iteration-1-images\logo.svg" alt="ty logo" />
+
+        <p className="siparisSonucYazi">
+          <span className="sariYazi">lezzetin yolda</span>
+          <br />
+          SİPARİŞ ALINDI
+          <hr />
+        </p>
+        <div className="siparisOzeti bej">
+          <p className="bold">Position Absolute Acı Pizza</p>
+          <div className="pizzaOzellik">
+          <p>
+            Boyut : <span className="bold">{response.boyut}</span>
+          </p>
+          <p>
+            Hamur : <span className="bold"> {response.hamurKalinlik}</span>
+          </p>
+          <p>
+            Ek Malzemeler :
+            <span className="bold">
+              {" "}
+              {response.malzemeler?.join(", ") || "Yok"}
+            </span>
+          </p>
+          </div>
+
+          <div className="siparisToplamOzet bold">
+            <p className="siparisToplam">Sipariş Toplamı</p>
+            <div className="siparisVerSecimler">
+              <p>Seçimler</p>
+              <p>{response.secimFiyat} ₺</p>
+            </div>
+            <div className="siparisVerToplam">
+              <p>Toplam</p>
+              <p>{response.toplamFiyat} ₺</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default OrderResult;
